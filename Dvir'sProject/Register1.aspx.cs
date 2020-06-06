aa402bb44034e5e683a75d9855265508168c9a07
DvirosL <57788665+DvirosL@users.noreply.github.com>
2020-06-06 20:39:19 +0300

    6666

--- Dvir'sProject/Register1.aspx.cs
+++ Dvir'sProject/Register1.aspx.cs
@@ -21,15 +21,16 @@ public partial class Register1 : System.Web.UI.Page
             string bday = Request.Form["age"];
             string gender = Request.Form["gender"];
             string phone = Request.Form["phone"];
-            string sql = "Select * from tbl_users where UserName='" + uname + "'";
-            if (MyAdoHelperAccess.IsExist(db, sql))
+            string sql = "Select * from tbl_users where uname='" + uname + "'";
+            if (!MyAdoHelperAccess.IsExist(db, sql))
             {
-                Response.Write("Username already exists");
-                Response.AddHeader("REFRESH", "2;URL=Register1.aspx");
+                MyAdoHelperAccess.DoQuery(db, "insert into tbl_users (uname, Fname, Lname, upass, email, bday, gender, cellNum, phoneNum) values ('" + uname + "', '" + fname + "', '" + lname + "', '" + passw + "', '" + email + "', '" + bday + "', '" + gender + "', '" + phone.Substring(0, 3) + "', '" + phone.Substring(3) + "');");
+                Response.Redirect("Login1.aspx");
             }
             else
             {
-                MyAdoHelperAccess.DoQuery(db, "insert into tbl_users (uname, Fname, Lname, upass, email, bday, gender, cellNum) values ('" + uname + "', '" + fname + "', '" + lname + "', '" + passw + "', '" + email + "', '" + bday + "', '" + gender + "', '" + phone + "');");
+                Response.Write("Username already exists");
+                Response.AddHeader("REFRESH", "2;URL=Register1.aspx");
             }
         }
     }
